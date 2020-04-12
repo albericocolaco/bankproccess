8707bbaaba2cb78274b16975fd3ca99a9575cf3a
Albérico Colaço <alberico.rodrigo@gmail.com>
2020-04-12 19:48:40 -0300

    Adjust code of log in oauth

--- OAuth/src/modules/oauth.module.ts
+++ OAuth/src/modules/oauth.module.ts
@@ -4,14 +4,13 @@ import * as jwt from "jsonwebtoken";
 import HttpException from "../exceptions/http.exception";
 import { environment } from "../environments/environment";
 import UserRepository from "../repository/user.repository";
-import { logger } from "../middlewares/logger.middleware";
 
 export class OAuthModule{
 
     public execute(req: Request, res: Response, next: NextFunction){
         let {username, password} = req.body;
         if (!(username && password)) {
-            next(new HttpException({code:'0001', httpCode: 401, message: 'Unauthorized'}))
+            next(new HttpException({code:'OA0001', httpCode: 401, message: 'Unauthorized'}))
         } else {
             UserRepository.getByDocument(username).then(user => {
                 console.log(user)
@@ -20,10 +19,10 @@ export class OAuthModule{
                     var response: OAuthResponseModel = {token: jwt.sign(bodyJWT, environment.privateKeyJWT, { expiresIn:  environment.jwtExpiresIn })}
                     res.send(response);
                 } else {
-                    next(new HttpException({code:'0002', httpCode: 403, message: 'Invalid'}))
+                    next(new HttpException({code:'OA0002', httpCode: 403, message: 'Invalid'}))
                 }
             }).catch(err => {
-                next(new HttpException({code:'0003', httpCode: 403, message: 'Invalid'}))
+                next(new HttpException({code:'OA0003', httpCode: 403, message: 'Invalid'}, err))
             });
         }
     }
